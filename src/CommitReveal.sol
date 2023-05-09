0f3ac85c9da06f629761f8ff277cb349d4d4f5f5
James Wenzel <wenzel.james.r@gmail.com>
2023-05-08 20:34:54 -0700

    initial commit

---
+++ src/CommitReveal.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract CommitReveal {
+    error InvalidCommitment();
+
+    uint256 constant INVALID_COMMITMENT_SELECTOR = 0xc06789fa;
+    uint256 constant COMMITMENT_LIFESPAN = 1 days;
+    uint256 constant COMMITMENT_DELAY = 1 minutes;
+
+    mapping(address committer => mapping(bytes32 commitmentHash => uint256 timestamp)) internal _commitments;
+
+    function commit(bytes32 commitmentHash) external {
+        ///@solidity memory-safe-assembly
+        assembly {
+            mstore(0, caller())
+            mstore(0x20, _commitments.slot)
+            let nestedMapSlot := keccak256(0, 0x40)
+            mstore(0, commitmentHash)
+            mstore(0x20, nestedMapSlot)
+            let finalSlot := keccak256(0, 0x40)
+            sstore(finalSlot, timestamp())
+        }
+    }
+
+    function getCommitment(address committer, bytes32 commitmentHash) public view returns (uint256 _timestamp) {
+        ///@solidity memory-safe-assembly
+        assembly {
+            mstore(0, committer)
+            mstore(0x20, _commitments.slot)
+            let nestedMapSlot := keccak256(0, 0x40)
+            mstore(0, commitmentHash)
+            mstore(0x20, nestedMapSlot)
+            let finalSlot := keccak256(0, 0x40)
+            _timestamp := sload(finalSlot)
+        }
+    }
+
+    function _validateCommitment(bytes32 salt, string calldata name) internal view {
+        bytes32 computedHash;
+        ///@solidity memory-safe-assembly
+        assembly {
+            mstore(0, salt)
+            mstore(0x20, calldataload(name.offset))
+            computedHash := keccak256(0, 0x40)
+        }
+        uint256 committedTimestamp = getCommitment(msg.sender, computedHash);
+        ///@solidity memory-safe-assembly
+        assembly {
+            let errBuffer := gt(sub(timestamp(), committedTimestamp), COMMITMENT_LIFESPAN)
+            errBuffer := or(errBuffer, lt(sub(timestamp(), committedTimestamp), COMMITMENT_DELAY))
+            if errBuffer {
+                mstore(0, INVALID_COMMITMENT_SELECTOR)
+                revert(0x1c, 0x20)
+            }
+        }
+    }
+}
--- src/Memecoin.sol
+++ src/Memecoin.sol
@@ -1,70 +1,153 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import "lib/openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
-import "lib/forge-std/src/interfaces/IERC20.sol";
-import "./interface/IUniswapV2Factory.sol";
+import {ERC20} from "solady/tokens/ERC20.sol";
+import {Ownable} from "solady/auth/Ownable.sol";
+import {IUniswapV2Factory} from "./interface/IUniswapV2Factory.sol";
+import {IUniswapV2Router} from "./interface/IUniswapV2Router.sol";
 
-error FailedToProvideLiquidity();
-error CallerNotOwner();
-error LiquidityLocked();
+contract Memecoin is ERC20, Ownable {
+    error FailedToProvideLiquidity();
+    error CallerNotOwner();
+    error LiquidityLocked();
+    error InvalidInitializationParams();
 
-contract Memecoin is ERC20Upgradeable {
+    uint256 private constant GET_PAIR_SELECTOR = 0xe6a43905;
+    uint256 private constant BALANCE_OF_SELECTOR = 0x70a08231;
+    uint256 private constant TRANSFER_SELECTOR = 0xa9059cbb;
+    uint256 private constant INVALID_INITIALIZATION_PARAMS_SELECTOR = 0x7676b397;
+    uint256 private constant LIQUIDITY_LOCKED_SELECTOR = 0x8f4e75b2;
+
     address private constant UNISWAP_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address private constant UNISWAP_FACTORY = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-    uint256 public TEAM_PERCENTAGE;
-    uint256 public LIQUIDITY_LOCKED_UNTIL;
-    address public DEPLOYER;
+    uint256 public immutable TEAM_BPS;
+    uint256 public immutable LIQUIDITY_LOCKED_UNTIL;
+    bytes32 immutable _NAME;
+    bytes32 immutable _SYMBOL;
 
-    function initialize (
-        string calldata name,
-        string calldata sym,
+    constructor(
+        string memory name_,
+        string memory sym,
         uint256 _totalSupply,
         address _deployer,
-        uint256 _teamPercentage,
+        uint256 _teamBps,
         uint256 liquidityLockPeriodInSeconds
-    ) initializer external payable {
-        TEAM_PERCENTAGE = _teamPercentage;
+    ) payable {
+        bytes32 _name;
+        bytes32 _symbol;
+        ///@solidity memory-safe-assembly
+        assembly {
+            let errBuffer := iszero(lt(_teamBps, 10000))
+
+            errBuffer := or(errBuffer, gt(mload(name_), 31))
+            errBuffer := or(errBuffer, gt(mload(sym), 31))
+            if errBuffer {
+                mstore(0, INVALID_INITIALIZATION_PARAMS_SELECTOR)
+                revert(0x1c, 0x20)
+            }
+            _name := mload(add(31, name_))
+            _symbol := mload(add(31, sym))
+        }
+        _NAME = _name;
+        _SYMBOL = _symbol;
+
+        TEAM_BPS = _teamBps;
         LIQUIDITY_LOCKED_UNTIL = block.timestamp + liquidityLockPeriodInSeconds;
-        DEPLOYER = _deployer;
-        __ERC20_init(name, sym);
+        _setOwner(_deployer);
 
-        uint256 fullTotalSupply = _totalSupply * 10 ** decimals();
-        uint256 teamTokens = (fullTotalSupply * TEAM_PERCENTAGE) / 100;
-        uint256 poolTokens = fullTotalSupply - teamTokens;
+        uint256 teamTokens; // = (fullTotalSupply * TEAM_BPS) / 100;
+        uint256 poolTokens; //= fullTotalSupply - teamTokens;
+        ///@solidity memory-safe-assembly
+        assembly {
+            let fullTotalSupply := mul(_totalSupply, 1000000000000000000)
+            teamTokens := div(mul(fullTotalSupply, _teamBps), 10000)
+            poolTokens := sub(fullTotalSupply, teamTokens)
+        }
 
-        _mint(DEPLOYER, teamTokens);
+        // _mint(_deployer, teamTokens);
+        // _mint(address(this), poolTokens);
+        // _approve(address(this), UNISWAP_ROUTER, poolTokens);
 
-        if (TEAM_PERCENTAGE == 100) return;
+        if (poolTokens > 0) {
+            (bool success,) = UNISWAP_ROUTER.call{value: msg.value}(
+                abi.encodeCall(
+                    IUniswapV2Router.addLiquidityETH, (address(this), poolTokens, 0, 0, address(this), block.timestamp)
+                )
+            );
 
-        _mint(address(this), poolTokens);
-        _approve(address(this), UNISWAP_ROUTER, poolTokens);
-
-        address liquidityRecipient = liquidityLockPeriodInSeconds == 0 ? DEPLOYER : address(this);
-
-        (bool success, ) = UNISWAP_ROUTER.call{value: msg.value}(
-            abi.encodeWithSelector(
-                0xf305d719,
-                address(this),
-                poolTokens,
-                0,
-                0,
-                liquidityRecipient,
-                block.timestamp
-            )
-        );
-
-        if (!success) revert FailedToProvideLiquidity();
+            if (!success) revert FailedToProvideLiquidity();
+        }
     }
 
-    function withdrawLP() external {
-        if (msg.sender != DEPLOYER) revert CallerNotOwner();
-        if (block.timestamp < LIQUIDITY_LOCKED_UNTIL) revert LiquidityLocked();
+    function name() public view override returns (string memory _name) {
+        bytes32 name_ = _NAME;
+        ///@solidity memory-safe-assembly
+        assembly {
+            // get the free memory pointer
+            let freePtr := mload(0x40)
+            // set _name to the free memory pointer
+            _name := freePtr
+            // update free pointer by two words
+            mstore(0x40, add(0x40, freePtr))
+            // write the length of the name to the last byte of the first word
+            // and the contents to the next word
+            mstore(add(_name, 0x1f), name_)
+        }
+    }
 
-        address pair = IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f).getPair(address(this), WETH);
-        uint256 balance = IERC20(pair).balanceOf(address(this));
-        pair.call(abi.encodeWithSelector(0xa9059cbb, DEPLOYER, balance));
+    function symbol() public view override returns (string memory _symbol) {
+        bytes32 symbol_ = _SYMBOL;
+        ///@solidity memory-safe-assembly
+        assembly {
+            let freePtr := mload(0x40)
+            _symbol := freePtr
+            // update free pointer by two words
+            mstore(0x40, add(0x40, freePtr))
+            // write the length of the symbol to the last byte of the first word
+            // and the contents to the next word
+            mstore(add(_symbol, 0x1f), symbol_)
+        }
+    }
+
+    function withdrawLP() external onlyOwner {
+        uint256 liquidityLockedUntil;
+        address owner = owner();
+        ///@solidity memory-safe-assembly
+        assembly {
+            function checkSuccess(status) {
+                if iszero(status) {
+                    returndatacopy(0, 0, returndatasize())
+                    revert(0, returndatasize())
+                }
+            }
+
+            // assert liquidity is unlocked
+            if gt(liquidityLockedUntil, timestamp()) {
+                mstore(0, LIQUIDITY_LOCKED_SELECTOR)
+                revert(0x1c, 0x20)
+            }
+            // cache free memory ptr
+            let freePtr := mload(0x40)
+
+            // get pair address
+            mstore(0, GET_PAIR_SELECTOR)
+            mstore(0x20, address())
+            mstore(0x40, WETH)
+            checkSuccess(call(gas(), UNISWAP_FACTORY, 0, 0x1c, 0x44, freePtr, 0x20))
+            let pair := mload(freePtr)
+
+            // get LP token balance
+            mstore(0, BALANCE_OF_SELECTOR)
+            checkSuccess(call(gas(), pair, 0, 0x1c, 0x24, freePtr, 0x20))
+            let tokenBalance := mload(freePtr)
+
+            // transfer tokens to owner
+            mstore(0, TRANSFER_SELECTOR)
+            mstore(0x20, owner)
+            mstore(0x40, tokenBalance)
+            checkSuccess(call(gas(), pair, 0, 0x1c, 0x44, 0, 0))
+        }
     }
 }
